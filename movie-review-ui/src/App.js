e0d51b5dacac72efadc479d5b9c7fb93995cc75a
Deividas Kakta <deividaskakta@gmail.com>
2021-05-28 18:31:04 +0300

    Add initial Header, Content and Footer components

--- movie-review-ui/src/App.js
+++ movie-review-ui/src/App.js
@@ -1,10 +1,15 @@
+import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
