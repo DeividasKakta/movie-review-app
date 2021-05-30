fbb1db2ee7d10f2bdc2f833ba289a56f9cd452a4
Deividas Kakta <deividaskakta@gmail.com>
2021-05-31 00:48:43 +0300

    Update LandingPage with initial card template

--- movie-review-ui/src/pages/LandingPage/LandingPage.js
+++ movie-review-ui/src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,55 @@
+import {Card, CardActionArea, CardContent, CardMedia, Grid, Hidden, makeStyles, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    card: {
+        display: 'flex',
+    },
+    cardDetails: {
+        flex: 1,
+    },
+    cardMedia: {
+        width: 160,
+    },
+});
 
 const LandingPage = () => {
+    const classes = useStyles();
 
     return (
-        <>
-            <h1>Landing page</h1>
-        </>
+        <main>
+            <Grid container spacing={4}>
+                <Grid item xs={12}>
+                    <Typography variant="h2">
+                        Landing page
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <CardActionArea component="a" href="#">
+                        <Card className={classes.card}>
+                            <div className={classes.cardDetails}>
+                                <CardContent>
+                                    <Typography component="h2" variant="h5">
+                                        Movie title
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                        2021
+                                    </Typography>
+                                    <Typography variant="subtitle1" paragraph>
+                                        Movie description
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="primary">
+                                        Continue reading...
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                            <Hidden xsDown>
+                                <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="Random"/>
+                            </Hidden>
+                        </Card>
+                    </CardActionArea>
+                </Grid>
+            </Grid>
+        </main>
     )
 }
 
