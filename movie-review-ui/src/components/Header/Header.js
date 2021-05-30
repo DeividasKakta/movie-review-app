56969f6d3d8dd4e701d09462399092d96fe9b5b2
Deividas Kakta <deividaskakta@gmail.com>
2021-05-31 00:51:49 +0300

    Update header with initial AppBar, add Home and Add new movie links

--- movie-review-ui/src/components/Header/Header.js
+++ movie-review-ui/src/components/Header/Header.js
@@ -1,9 +1,49 @@
+import {AppBar, Button, IconButton, Link, makeStyles, Toolbar, Typography} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu"
+import {NavLink} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    link: {
+        margin: theme.spacing(1, 1.5),
+    },
+    active: {
+        fontSize: theme.spacing(2)
+    }
+}));
 
 const Header = () => {
+    const classes = useStyles();
 
     return (
         <>
-            <h1>Header</h1>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Movie Review
+                    </Typography>
+                    <Link variant="button" color="inherit" exact to="/"
+                          className={classes.link} activeClassName={classes.active} component={NavLink}>
+                        Home
+                    </Link>
+                    <Link variant="button" color="inherit" to="/movies/create"
+                          className={classes.link} activeClassName={classes.active} component={NavLink}>
+                        Add new movie
+                    </Link>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
