1a9ef5169d6a006460b1edd159984ac3334e0b95
Deividas Kakta <deividaskakta@gmail.com>
2021-05-31 00:47:50 +0300

    Create custom material-ui theme

--- movie-review-ui/src/App.js
+++ movie-review-ui/src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 import {BrowserRouter as Router} from "react-router-dom";
-import {Container, makeStyles} from "@material-ui/core";
+import {Container, createMuiTheme, makeStyles, MuiThemeProvider} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,18 +12,37 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#62727b',
+            main: '#37474f',
+            dark: '#102027',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#e1ffb1',
+            main: '#aed581',
+            dark: '#7da453',
+            contrastText: '#000',
+        },
+    },
+})
+
 function App() {
     const classes = useStyles();
 
     return (
         <Router>
-            <div className={classes.root}>
-                <Header/>
-                <Container maxWidth="lg">
-                    <Content/>
-                </Container>
-                <Footer/>
-            </div>
+            <MuiThemeProvider theme={theme}>
+                <div className={classes.root}>
+                    <Header/>
+                    <Container maxWidth="lg">
+                        <Content/>
+                    </Container>
+                    <Footer/>
+                </div>
+            </MuiThemeProvider>
         </Router>
     );
 }
