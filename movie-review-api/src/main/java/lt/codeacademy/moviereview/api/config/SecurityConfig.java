dffa67772b1d75fedc4f5ecb1af0cbac15b66078
Deividas Kakta <deividaskakta@gmail.com>
2021-06-01 17:28:35 +0300

    REST: Create initial SecurityConfig file with disabled security

---
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package lt.codeacademy.moviereview.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/h2/**");    //h2 console
+    }
+}
