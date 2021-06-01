e44f7fdda4f7fe191abd8dae8027cb54dcdce44e
Deividas Kakta <deividaskakta@gmail.com>
2021-06-01 17:29:43 +0300

    REST: Update initial-data.sql with admin as user

--- movie-review-api/src/main/resources/database/initial-data.sql
+++ movie-review-api/src/main/resources/database/initial-data.sql
@@ -2,3 +2,6 @@ INSERT INTO movie (id, title, description, cast, release_date) VALUES
         ('0c9d9f98-c90a-459d-af35-0ff5c2105127', 'Godzilla vs. Kong',
         'The epic next chapter in the cinematic Monsterverse pits two of the greatest icons in motion picture history against one another - the fearsome Godzilla and the mighty Kong - with humanity caught in the balance.',
         'Alexander Skarsgård, Millie Bobby Brown, Rebecca Hall ', '2021-03-31');
+
+INSERT INTO user (id, username, password) VALUES
+    ('af4abdf2-9ba9-11eb-a8b3-0242ac130799', 'admin', '{bcrypt}$2y$12$jTtuvT/AazGVykRoiOP8JOQmoCsYrk.QpwpaVKVreNWJZhTDvMJom'); --admin
