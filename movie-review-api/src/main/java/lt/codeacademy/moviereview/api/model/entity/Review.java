eaa24abd3ebe0c2250052ad33cb3f214d4da452c
Deividas Kakta <deividaskakta@gmail.com>
2021-06-01 14:49:01 +0300

    REST: Create Review entity

---
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/model/entity/Review.java
@@ -0,0 +1,36 @@
+package lt.codeacademy.moviereview.api.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "VARCHAR(36)", updatable = false)
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Movie movie;
+
+    @NotBlank
+    @Size(min = 5, max = 1000)
+    @Column(columnDefinition = "VARCHAR(1000)")
+    private String content;
+
+    @Min(1)
+    @Max(10)
+    @NotNull
+    private Integer rating;
+}
