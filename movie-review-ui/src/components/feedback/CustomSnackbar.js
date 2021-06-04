d92eef8e4b2a837d4600431cdcbd4ba8f597a3bf
Deividas Kakta <deividaskakta@gmail.com>
2021-06-04 16:37:34 +0300

    React: create custom snackbar for alerts

---
+++ movie-review-ui/src/components/feedback/CustomSnackbar.js
@@ -0,0 +1,12 @@
+import MuiAlert from '@material-ui/lab/Alert';
+import {Snackbar} from "@material-ui/core";
+
+const CustomSnackbar = ({open, duration, handleClose, elevation, variant, message, severity}) => (
+    <Snackbar open={open} autoHideDuration={duration} onClose={handleClose}>
+        <MuiAlert onClose={handleClose} elevation={elevation} variant={variant} severity={severity}>
+            {message}
+        </MuiAlert>
+    </Snackbar>
+);
+
+export default CustomSnackbar;
