36acf8cfad130d0d1986a8019a7b5a06edc372aa
Deividas Kakta <deividaskakta@gmail.com>
2021-06-04 16:42:17 +0300

    React: update MovieForm with customSnackbar and formikInput implementation, minor MovieCreationPage changes

--- movie-review-ui/src/components/forms/MovieForm/MovieForm.js
+++ movie-review-ui/src/components/forms/MovieForm/MovieForm.js
@@ -6,14 +6,14 @@ import {
     FormControl,
     FormHelperText,
     Grid,
-    InputLabel,
     makeStyles,
-    OutlinedInput,
-    Paper,
-    Typography
+    Paper
 } from "@material-ui/core";
 import {KeyboardDatePicker} from "@material-ui/pickers";
 import {createMovie} from "../../../api/moviesApi";
+import CustomSnackbar from "../../feedback/CustomSnackbar";
+import {useState} from "react";
+import OutlinedFormikInput from "../../inputs/OutlinedFormikInput";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -36,6 +36,36 @@ const MovieForm = () => {
     const classes = useStyles()
     const now = new Date()
 
+    const [openError, setOpenError] = useState(false);
+    const [openSuccess, setOpenSuccess] = useState(false);
+
+    const handleErrorClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenError(false);
+    };
+
+    const handleSuccessClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenSuccess(false);
+    };
+
+    const postForm = (data, {setSubmitting}) => {
+        setSubmitting(true)
+
+        createMovie(data)
+            .then(() => setOpenSuccess(true))
+            .catch(() => setOpenError(true))
+            .finally(() => {
+                setTimeout(() => {
+                    setSubmitting(false)
+                }, 300)
+            })
+    }
+
     return (
         <Formik initialValues={{
             title: '',
@@ -43,38 +73,22 @@ const MovieForm = () => {
             cast: '',
             releaseDate: now
         }}
-                onSubmit={
-                    (values, helpers) => {
-                        helpers.setSubmitting(true)
-
-                        createMovie(values)
-                            .then(() => console.log("success"))
-                            .catch((errors) => console.log(errors))
-                            .finally(() => helpers.setSubmitting(false))
-
-                    }
-                }
+                onSubmit={postForm}
                 validationSchema={validationSchema}
         >
             {props => (
                 <Container maxWidth="sm">
                     <Paper elevation={3} className={classes.root}>
-                        <Typography variant="h4" align="center">
-                            Add new movie
-                        </Typography>
                         <Form>
                             <Grid container spacing={2}>
-                                <Grid item xs={12} md={6}>
-                                    <FormControl error={props.touched.title && !!props.errors.title} fullWidth
-                                                 variant="outlined" margin="normal">
-                                        <InputLabel htmlFor="title">Title</InputLabel>
-                                        <Field id="title" name="title" label="Title" placeholder="Type..."
-                                               as={OutlinedInput}/>
-                                        <ErrorMessage name="title" component={FormHelperText}/>
-                                    </FormControl>
+                                <Grid item xs={6}>
+                                    <OutlinedFormikInput name="title"
+                                                         label="Title"
+                                                         error={props.touched.title && !!props.errors.title}
+                                                         placeholder="Enter movie title..."/>
                                 </Grid>
 
-                                <Grid item xs={12} md={6}>
+                                <Grid item xs={6}>
                                     <FormControl error={props.touched.releaseDate && !!props.errors.releaseDate}
                                                  variant="standard" margin="normal">
                                         <Field id="releaseDate" name="releaseDate" label="Release date"
@@ -88,45 +102,49 @@ const MovieForm = () => {
                                 </Grid>
 
                                 <Grid item xs={12}>
-                                    <FormControl error={props.touched.description && !!props.errors.description}
-                                                 fullWidth
-                                                 variant="outlined" margin="normal">
-                                        <InputLabel htmlFor="description">Description</InputLabel>
-                                        <Field id="description" name="description" label="Description"
-                                               placeholder="Type..."
-                                               as={OutlinedInput}/>
-                                        <ErrorMessage name="description" component={FormHelperText}/>
-                                    </FormControl>
+                                    <OutlinedFormikInput name="description"
+                                                         label="Description"
+                                                         error={props.touched.description && !!props.errors.description}
+                                                         placeholder="Enter movie description..."
+                                                         multiline
+                                                         rows={4}/>
                                 </Grid>
 
                                 <Grid item xs={12}>
-                                    <FormControl error={props.touched.cast && !!props.errors.cast} fullWidth
-                                                 variant="outlined" margin="normal">
-                                        <InputLabel htmlFor="cast">Cast</InputLabel>
-                                        <Field id="cast" name="cast" label="Cast" placeholder="Type..."
-                                               as={OutlinedInput}/>
-                                        <ErrorMessage name="description" component={FormHelperText}/>
-                                    </FormControl>
+                                    <OutlinedFormikInput name="cast"
+                                                         label="Cast"
+                                                         error={props.touched.cast && !!props.errors.cast}
+                                                         placeholder="Enter movie cast..."
+                                                         multiline
+                                                         rows={2}/>
                                 </Grid>
 
                             </Grid>
 
+                            <Button variant="contained"
+                                    fullWidth
+                                    color="primary"
+                                    type="submit"
+                                    disabled={props.isSubmitting}>Submit</Button>
 
-                            {/*<div>*/}
-                            {/*    <FormikInput name="surname"*/}
-                            {/*                 label="Surname"*/}
-                            {/*                 error={props.touched.surname && !!props.errors.surname}/>*/}
-                            {/*</div>*/}
-
-
-                            {!props.isSubmitting ? <Button variant="contained"
-                                                           fullWidth
-                                                           color="primary"
-                                                           type="submit">Submit</Button> :
-                                <span>Submitting...</span>}
                         </Form>
                     </Paper>
 
+                    <CustomSnackbar open={openError}
+                                    duration={5000}
+                                    handleClose={handleErrorClose}
+                                    message="Error creating movie"
+                                    elevation={3}
+                                    variant="filled"
+                                    severity="error"/>
+
+                    <CustomSnackbar open={openSuccess}
+                                    duration={5000}
+                                    handleClose={handleSuccessClose}
+                                    message="Created successfully!"
+                                    elevation={3}
+                                    variant="filled"
+                                    severity="success"/>
                 </Container>
             )}
         </Formik>
--- movie-review-ui/src/pages/MovieCreationPage/MovieCreationPage.js
+++ movie-review-ui/src/pages/MovieCreationPage/MovieCreationPage.js
@@ -1,10 +1,20 @@
 import MovieForm from "../../components/forms/MovieForm/MovieForm";
+import {Grid, Typography} from "@material-ui/core";
 
 const MovieCreationPage = () => {
 
     return (
         <>
-            <MovieForm/>
+            <main>
+                <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                        <Typography variant="h2" align="center">
+                            Add new movie
+                        </Typography>
+                    </Grid>
+                    <MovieForm/>
+                </Grid>
+            </main>
         </>
     )
 }
