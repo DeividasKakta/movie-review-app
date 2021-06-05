6fe66fd8e50c3606cf4b41e185063ef31146e3fd
Deividas Kakta <deividaskakta@gmail.com>
2021-06-05 19:06:29 +0300

    React: Add fetchMovieById method to moviesApi

--- movie-review-ui/src/api/moviesApi.js
+++ movie-review-ui/src/api/moviesApi.js
@@ -2,6 +2,8 @@ import HTTP from "./index";
 
 const fetchMovies = () => HTTP.get('/movies')
 
+const fetchMovieById = (id) => HTTP.get('/movies/' + id)
+
 const createMovie = (movie) => HTTP.post('/movies', movie)
 
-export {fetchMovies, createMovie}
+export {fetchMovies, createMovie, fetchMovieById}
