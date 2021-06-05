d15939e12df2e5576a84c22bd3d5a5a7b414bad8
Deividas Kakta <deividaskakta@gmail.com>
2021-06-05 19:00:21 +0300

    REST: Update ReviewController getReviewsByMovieId method to return ReviewDto

--- movie-review-api/src/main/java/lt/codeacademy/moviereview/api/controller/ReviewController.java
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/controller/ReviewController.java
@@ -1,14 +1,18 @@
 package lt.codeacademy.moviereview.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import lt.codeacademy.moviereview.api.model.dto.ReviewDto;
 import lt.codeacademy.moviereview.api.model.entity.Review;
 import lt.codeacademy.moviereview.api.service.MovieService;
 import lt.codeacademy.moviereview.api.service.ReviewService;
+import lt.codeacademy.moviereview.api.utils.mapper.ReviewDtoMapper;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static lt.codeacademy.moviereview.api.config.Endpoint.*;
 
@@ -18,15 +22,22 @@ import static lt.codeacademy.moviereview.api.config.Endpoint.*;
 public class ReviewController {
     private final ReviewService reviewService;
     private final MovieService movieService;
+    private final ReviewDtoMapper reviewDtoMapper;
 
     @GetMapping(REVIEW_BY_MOVIE)
-    public List<Review> getReviewsByMovieId(@PathVariable(UUID) UUID uuid) {
-        return reviewService.getReviewsByMovieId(uuid);
+    public List<ReviewDto> getReviewsByMovieId(@PathVariable(UUID) UUID uuid) {
+        List<Review> reviewsByMovieId = reviewService.getReviewsByMovieId(uuid);
+
+        return reviewsByMovieId.stream()
+                .map(reviewDtoMapper::mapToDto)
+                .sorted(Comparator.comparing(ReviewDto::getReviewDate))
+                .collect(Collectors.toList());
     }
 
     @PostMapping
     public void createReview(@Valid @RequestBody Review review, @RequestParam(name = MOVIE) UUID uuid) {
         review.setMovie(movieService.getMovieById(uuid));
+
         reviewService.createReview(review);
     }
 
