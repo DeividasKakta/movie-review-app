0d5593d83bb67e640b08d40ef64767a2f939cfeb
Deividas Kakta <deividaskakta@gmail.com>
2021-06-05 18:58:25 +0300

    REST: Create ReviewDto and it's mapper

---
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/model/dto/ReviewDto.java
@@ -0,0 +1,20 @@
+package lt.codeacademy.moviereview.api.model.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Builder
+@Getter
+public class ReviewDto {
+    private final UUID reviewId;
+    private final UUID movieId;
+    private final UUID userId;
+    private final String username;
+    private final Date reviewDate;
+    private final Integer rating;
+    private final String content;
+
+}
---
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/utils/mapper/ReviewDtoMapper.java
@@ -0,0 +1,21 @@
+package lt.codeacademy.moviereview.api.utils.mapper;
+
+import lt.codeacademy.moviereview.api.model.dto.ReviewDto;
+import lt.codeacademy.moviereview.api.model.entity.Review;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReviewDtoMapper {
+
+    public ReviewDto mapToDto(Review review) {
+        return ReviewDto.builder()
+                .reviewId(review.getId())
+                .movieId(review.getMovie().getId())
+                .userId(review.getUser().getId())
+                .reviewDate(review.getDate())
+                .username(review.getUser().getUsername())
+                .content(review.getContent())
+                .rating(review.getRating())
+                .build();
+    }
+}
