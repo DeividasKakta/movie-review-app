86c25ab4de6d17a0119af5390271c0d841c7a196
Deividas Kakta <deividaskakta@gmail.com>
2021-06-08 20:19:18 +0300

    React: Small design changes in LandingPage

--- movie-review-ui/src/pages/LandingPage/LandingPage.js
+++ movie-review-ui/src/pages/LandingPage/LandingPage.js
@@ -1,18 +1,28 @@
 import {Card, CardActionArea, CardContent, CardMedia, Grid, Hidden, makeStyles, Typography} from "@material-ui/core";
 import {useEffect, useState} from "react";
 import {fetchMovies} from "../../api/moviesApi";
+import {Link} from "react-router-dom";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     card: {
         display: 'flex',
     },
     cardDetails: {
         flex: 1,
+        overflowWrap: "anywhere"
     },
     cardMedia: {
-        width: 160,
+        width: 200,
     },
-});
+    centeredGrid: {
+        display: 'grid',
+        alignItems: 'center'
+    },
+    gridContainer: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1)
+    }
+}));
 
 const LandingPage = () => {
     const classes = useStyles();
@@ -20,13 +30,12 @@ const LandingPage = () => {
 
     useEffect(() => {
         fetchMovies()
-            .then(({ data }) => setMovies(data))
-            .catch(error => console.log("error", error))
+            .then(({data}) => setMovies(data))
     }, [])
 
     return (
         <main>
-            <Grid container spacing={4}>
+            <Grid container spacing={4} className={classes.gridContainer}>
                 <Grid item xs={12}>
                     <Typography variant="h2">
                         Landing page
@@ -34,59 +43,34 @@ const LandingPage = () => {
                 </Grid>
 
                 {movies.map((movie) => (
-                    <Grid key={movie.id} item xs={12} md={6}>
-                        <CardActionArea component="a" href="#">
+                    <Grid key={movie.movieId} item xs={12} md={6} className={classes.centeredGrid}>
+                        <CardActionArea  component={Link} to={"/movies/" + movie.movieId}>
                             <Card className={classes.card}>
                                 <div className={classes.cardDetails}>
                                     <CardContent>
                                         <Typography component="h2" variant="h5">
                                             {movie.title}
+                                            {movie.averageRating?.toFixed(1)}
                                         </Typography>
                                         <Typography variant="subtitle1" color="textSecondary">
-                                            {movie.releaseDate}
+                                            {new Date(movie.releaseYear).getFullYear()}
                                         </Typography>
-                                        <Typography variant="subtitle1" paragraph>
-                                            {movie.description}
-                                        </Typography>
+                                            <Typography variant="subtitle1" paragraph>
+                                                {movie.description}
+                                            </Typography>
                                         <Typography variant="subtitle1" color="primary">
                                             {movie.cast}
                                         </Typography>
                                     </CardContent>
                                 </div>
                                 <Hidden xsDown>
-                                    <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="Random"/>
+                                    <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random"
+                                               title="Random"/>
                                 </Hidden>
                             </Card>
                         </CardActionArea>
                     </Grid>
                 ))}
-
-
-                <Grid item xs={12} md={6}>
-                    <CardActionArea component="a" href="#">
-                        <Card className={classes.card}>
-                            <div className={classes.cardDetails}>
-                                <CardContent>
-                                    <Typography component="h2" variant="h5">
-                                        Movie title
-                                    </Typography>
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        2021
-                                    </Typography>
-                                    <Typography variant="subtitle1" paragraph>
-                                        Movie description
-                                    </Typography>
-                                    <Typography variant="subtitle1" color="primary">
-                                        Continue reading...
-                                    </Typography>
-                                </CardContent>
-                            </div>
-                            <Hidden xsDown>
-                                <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="Random"/>
-                            </Hidden>
-                        </Card>
-                    </CardActionArea>
-                </Grid>
             </Grid>
         </main>
     )
