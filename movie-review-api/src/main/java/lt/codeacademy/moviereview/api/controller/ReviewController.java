1da781cae991673c1568d7b5894fa8acf3e65226
Deividas Kakta <deividaskakta@gmail.com>
2021-06-09 16:51:38 +0300

    REST: Update createReview method with user from context with a workaround AuthenticationPrincipal fix

--- movie-review-api/src/main/java/lt/codeacademy/moviereview/api/controller/ReviewController.java
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/controller/ReviewController.java
@@ -3,9 +3,13 @@ package lt.codeacademy.moviereview.api.controller;
 import lombok.RequiredArgsConstructor;
 import lt.codeacademy.moviereview.api.model.dto.ReviewDto;
 import lt.codeacademy.moviereview.api.model.entity.Review;
+import lt.codeacademy.moviereview.api.model.entity.User;
 import lt.codeacademy.moviereview.api.service.MovieService;
 import lt.codeacademy.moviereview.api.service.ReviewService;
+import lt.codeacademy.moviereview.api.service.UserService;
 import lt.codeacademy.moviereview.api.utils.mapper.ReviewDtoMapper;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,12 +21,13 @@ import java.util.stream.Collectors;
 import static lt.codeacademy.moviereview.api.config.Endpoint.*;
 
 @RestController
-@RequestMapping(API_ROOT + REVIEWS)
+@RequestMapping(REVIEWS)
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
     private final MovieService movieService;
     private final ReviewDtoMapper reviewDtoMapper;
+    private final UserService userService;
 
     @GetMapping(REVIEW_BY_MOVIE)
     public List<ReviewDto> getReviewsByMovieId(@PathVariable(UUID) UUID uuid) {
@@ -35,7 +40,15 @@ public class ReviewController {
     }
 
     @PostMapping
-    public void createReview(@Valid @RequestBody Review review, @RequestParam(name = MOVIE) UUID uuid) {
+    @PreAuthorize("hasRole('USER')")
+    public void createReview(@Valid @RequestBody Review review,
+                             @RequestParam(name = MOVIE) UUID uuid,
+                             @AuthenticationPrincipal String username) {
+//        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User user = userService.loadUserByUsername(username);
+
+        review.setUser(user);
         review.setMovie(movieService.getMovieById(uuid));
 
         reviewService.createReview(review);
