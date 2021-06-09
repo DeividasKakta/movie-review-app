307da804d39521295dcd41c2494a0ce4201d0f18
Deividas Kakta <deividaskakta@gmail.com>
2021-06-09 22:24:11 +0300

    REST: Add antMatchers to allow public endpoints

--- movie-review-api/src/main/java/lt/codeacademy/moviereview/api/config/SecurityConfig.java
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and()
                 .authorizeRequests()
+                .antMatchers("/reviews/movie/**", "/movies/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .exceptionHandling()
