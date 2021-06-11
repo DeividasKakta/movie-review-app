9307e6d974aa0731f59bd79c91a28858667a8991
Deividas Kakta <deividaskakta@gmail.com>
2021-06-11 12:45:37 +0300

    React: Add action prop to custom snackbar for customisable action option

--- movie-review-ui/src/components/feedback/CustomSnackbar.js
+++ movie-review-ui/src/components/feedback/CustomSnackbar.js
@@ -1,9 +1,9 @@
 import MuiAlert from '@material-ui/lab/Alert';
 import {Snackbar} from "@material-ui/core";
 
-const CustomSnackbar = ({open, duration, handleClose, elevation, variant, message, severity}) => (
+const CustomSnackbar = ({open, duration, handleClose, elevation, variant, message, severity, action}) => (
     <Snackbar open={open} autoHideDuration={duration} onClose={handleClose}>
-        <MuiAlert onClose={handleClose} elevation={elevation} variant={variant} severity={severity}>
+        <MuiAlert onClose={handleClose} elevation={elevation} variant={variant} severity={severity} action={action}>
             {message}
         </MuiAlert>
     </Snackbar>
