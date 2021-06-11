229ddad70ba603ffa3572bd3cf993cb15cff06a9
Deividas Kakta <deividaskakta@gmail.com>
2021-06-11 10:37:19 +0300

    React: Add initial implementation for top movies

--- movie-review-ui/src/api/moviesApi.js
+++ movie-review-ui/src/api/moviesApi.js
@@ -6,4 +6,6 @@ const fetchRatedMovieById = (id) => HTTP.get('/movies/' + id)
 
 const createMovie = (movie) => HTTP.post('/movies', movie)
 
-export {fetchMovies, createMovie, fetchRatedMovieById}
+const fetchTopMovies = () => HTTP.get('/movies/top')
+
+export {fetchMovies, createMovie, fetchRatedMovieById, fetchTopMovies}
--- movie-review-ui/src/pages/TopMoviesPage/TopMoviesPage.js
+++ movie-review-ui/src/pages/TopMoviesPage/TopMoviesPage.js
@@ -1,12 +1,77 @@
-import {Typography} from "@material-ui/core";
+import {
+    CardMedia, makeStyles,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+    withStyles
+} from "@material-ui/core";
+import {useEffect, useState} from "react";
+import {fetchTopMovies} from "../../api/moviesApi";
 
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: theme.spacing(3),
+    },
+}))(TableCell);
+
+const useStyles = makeStyles((theme) => ({
+    cardMedia: {
+        width: theme.spacing(5),
+        height: theme.spacing(8)
+    },
+    mainHeader: {
+        marginBottom: theme.spacing(3),
+    }
+}))
 
 const TopMoviesPage = () => {
+    const classes = useStyles()
+    const [topMovies, setTopMovies] = useState([])
 
+
+    useEffect(() => {
+        fetchTopMovies()
+            .then(({data}) => setTopMovies(data))
+    }, [])
 
     return (
         <>
-            <Typography variant="h1">Top movies</Typography>
+            <Typography variant="h1" align="center" className={classes.mainHeader}>Top movies</Typography>
+
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell colSpan={2} align="center">Title</StyledTableCell>
+                            <StyledTableCell align="right">Release year</StyledTableCell>
+                            <StyledTableCell align="right">Rating</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {topMovies.map((movie) => (
+                            <TableRow key={movie.movieId}>
+                                <StyledTableCell component="th" scope="row">
+                                    <CardMedia className={classes.cardMedia} image={movie.picture}
+                                               title="Random"/>
+                                </StyledTableCell>
+                                <StyledTableCell align="left">{movie.title}</StyledTableCell>
+                                <StyledTableCell align="right">{new Date(movie.releaseDate).getFullYear()}</StyledTableCell>
+                                <StyledTableCell align="right">{movie.averageRating?.toFixed(1)}</StyledTableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
