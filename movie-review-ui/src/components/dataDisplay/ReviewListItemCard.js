81c9b328f4aba61943184f46bd505b94718eb306
Deividas Kakta <deividaskakta@gmail.com>
2021-06-11 17:30:43 +0300

    React: Update review item design

--- movie-review-ui/src/components/dataDisplay/ReviewListItemCard.js
+++ movie-review-ui/src/components/dataDisplay/ReviewListItemCard.js
@@ -1,8 +1,20 @@
-import {Button, Card, CardContent, Divider, Grid, Hidden, ListItem, makeStyles, Typography} from "@material-ui/core";
+import {
+    Avatar,
+    Button,
+    Card,
+    CardContent,
+    Divider,
+    Grid,
+    Hidden,
+    ListItem,
+    makeStyles,
+    Typography
+} from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import {useSelector} from "react-redux";
 import {loggedInUser} from "../../store/slices/userSlice";
+import moment from "moment";
 
 const useStyles = makeStyles((theme) => ({
     listItem: {
@@ -15,14 +27,37 @@ const useStyles = makeStyles((theme) => ({
     leftItem: {
         flexGrow: 1
     },
-    itemContent: {
-        padding: theme.spacing(1)
+    root: {
+        paddingBottom: 0
     },
     verticalDivider: {
         marginRight: "-1px"
     },
     mainCard: {
         flexGrow: 1
+    },
+    userItem: {
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginBottom: theme.spacing(1)
+    },
+    actionIcons: {
+        paddingTop: 0
+    },
+    userArea: {
+        backgroundColor: theme.palette.primary.light,
+    },
+    avatarColor: {
+        backgroundColor: theme.palette.secondary.dark
+    },
+    usernameTextColor: {
+        color: theme.palette.common.white
+    },
+    deleteButton: {
+        color: theme.palette.error.dark
+    },
+    userHeader: {
+        paddingBottom: 0
     }
 }))
 
@@ -35,29 +70,43 @@ const ReviewListItemCard = ({username, rating, date, content, handleOnEditReview
 
             <Card className={classes.mainCard}>
                 <Grid container>
-                    <Grid item xs={12} sm={3} md={2}>
-                        <CardContent className={classes.itemContent}>
+                    <Grid item xs={12} sm={2} md={1} className={classes.userArea}>
+                        <CardContent style={{paddingBottom: 8}}>
 
                             <Hidden xsDown>
-                                <Typography variant="subtitle2" noWrap>
-                                    {username}
-                                </Typography>
+                                <div>
+                                    <div>
+                                        <Avatar className={`${classes.userItem} ${classes.avatarColor}`}>
+                                            {username?.substring(0, 1).toUpperCase()}
+                                        </Avatar>
+                                    </div>
+
+                                    <Typography variant="subtitle2" align="center" className={classes.usernameTextColor}
+                                                noWrap>
+                                        {username}
+                                    </Typography>
+                                </div>
+
                             </Hidden>
 
                             <Hidden smUp>
                                 <div className={classes.reviewHeader}>
-                                    <Typography variant="subtitle2" display="inline" className={classes.leftItem}>
+                                    <Typography variant="subtitle2"
+                                                display="inline"
+                                                className={`${classes.usernameTextColor} ${classes.leftItem}`}>
                                         {username}
                                     </Typography>
                                     {
                                         currentUser?.username === username &&
-                                        <Button color="secondary" onClick={handleOnEditReview}>
+                                        <Button color="secondary" className={classes.actionIcons}
+                                                onClick={handleOnEditReview}>
                                             <EditIcon/>
                                         </Button>
                                     }
                                     {
                                         (currentUser?.username === username || currentUser?.roles.includes('ADMIN')) &&
-                                        <Button color="primary" onClick={handleOnDeleteReview}>
+                                        <Button className={`${classes.deleteButton} ${classes.actionIcons}`}
+                                                onClick={handleOnDeleteReview}>
                                             <DeleteIcon/>
                                         </Button>
                                     }
@@ -78,31 +127,39 @@ const ReviewListItemCard = ({username, rating, date, content, handleOnEditReview
                         </Grid>
                     </Hidden>
 
-                    <Grid item xs={12} sm={9} md={10}>
-                        <CardContent className={classes.itemContent}>
+                    <Grid item xs={12} sm={10} md={11}>
+                        <CardContent>
 
                             <div className={classes.reviewHeader}>
-                                <Typography variant="subtitle2" display="inline" className={classes.leftItem}>
-                                    Rating: {rating}/10
+                                <Typography variant="subtitle2" display="inline" component="pre">
+                                    {"Rating: "}
+                                </Typography>
+                                <Typography variant="subtitle2" color="textSecondary" component="span" display="inline"
+                                            className={classes.leftItem}>
+                                    {rating}/10
                                 </Typography>
 
                                 <Hidden xsDown>
                                     {
                                         currentUser?.username === username &&
-                                        <Button color="secondary" onClick={handleOnEditReview}>
+                                        <Button className={classes.actionIcons} color="secondary"
+                                                onClick={handleOnEditReview}>
                                             <EditIcon/>
                                         </Button>
                                     }
                                     {
                                         (currentUser?.username === username || currentUser?.roles.includes('ADMIN')) &&
-                                        <Button color="primary" onClick={handleOnDeleteReview}>
+                                        <Button className={`${classes.deleteButton} ${classes.actionIcons}`}
+                                                color="primary" onClick={handleOnDeleteReview}>
                                             <DeleteIcon/>
                                         </Button>
                                     }
                                 </Hidden>
 
                                 <Typography variant="subtitle1" color="textSecondary" align="right" display="inline">
-                                    {date}
+                                    {
+                                        moment(date)?.format("YYYY-MM-DD HH:mm")
+                                    }
                                 </Typography>
                             </div>
 
