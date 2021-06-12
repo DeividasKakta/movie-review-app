b4fed434631f87f6682f59a73269c5118b54689a
Deividas Kakta <deividaskakta@gmail.com>
2021-06-12 15:47:07 +0300

    React: Update formik input component with dynamic translations

--- movie-review-ui/src/components/inputs/OutlinedFormikInput.js
+++ movie-review-ui/src/components/inputs/OutlinedFormikInput.js
@@ -1,12 +1,22 @@
 import {FormControl, FormHelperText, InputLabel, OutlinedInput} from "@material-ui/core";
 import {ErrorMessage, Field} from "formik";
+import {useTranslation} from "react-i18next";
 
-const OutlinedFormikInput = ({name, label, error, placeholder = "Type...", ...props}) => (
-    <FormControl error={error} fullWidth variant="outlined" margin="normal">
-        <InputLabel htmlFor={name}>{label}</InputLabel>
-        <Field id={name} name={name} label={label} placeholder={placeholder} {...props} as={OutlinedInput}/>
-        <ErrorMessage name={name} component={FormHelperText}/>
-    </FormControl>
-)
+const OutlinedFormikInput = ({name, label, error, placeholder = "Type...", ...props}) => {
+    const {t, i18n} = useTranslation()
+
+    return (
+        <FormControl error={error} fullWidth variant="outlined" margin="normal" key={i18n.language}>
+            <InputLabel htmlFor={name}>{label}</InputLabel>
+            <Field id={name} name={name} label={label}
+                   placeholder={placeholder} {...props} as={OutlinedInput}/>
+            <ErrorMessage name={name} component={FormHelperText}>
+                {
+                    (msg) => <FormHelperText>{t(msg)}</FormHelperText>
+                }
+            </ErrorMessage>
+        </FormControl>
+    )
+}
 
 export default OutlinedFormikInput;
