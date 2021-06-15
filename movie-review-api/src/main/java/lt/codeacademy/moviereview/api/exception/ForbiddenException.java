115d0f5efcf68b6b823c3081999194c0b309f471
Deividas Kakta <deividaskakta@gmail.com>
2021-06-15 21:57:42 +0300

    REST: Refactor Unauthorize exception to Forbidden

--- movie-review-api/src/main/java/lt/codeacademy/moviereview/api/exception/ForbiddenException.java
+++ movie-review-api/src/main/java/lt/codeacademy/moviereview/api/exception/ForbiddenException.java
@@ -3,6 +3,6 @@ package lt.codeacademy.moviereview.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends RuntimeException{
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException{
 }
