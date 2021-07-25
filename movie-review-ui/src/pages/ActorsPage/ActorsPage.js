92e14034c67fe84e5522382c8b1d9ad362c1c9d5
Deividas Kakta <deividaskakta@gmail.com>
2021-07-25 12:30:43 +0300

    v1.1: Add actor deletion functionality

--- movie-review-ui/src/pages/ActorsPage/ActorsPage.js
+++ movie-review-ui/src/pages/ActorsPage/ActorsPage.js
@@ -1,5 +1,9 @@
 import {
-    CardMedia, makeStyles,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogTitle,
+    makeStyles,
     Paper,
     Table,
     TableBody,
@@ -7,12 +11,17 @@ import {
     TableContainer,
     TableHead,
     TableRow,
-    Typography, withStyles
+    Typography,
+    withStyles
 } from "@material-ui/core";
 import moment from "moment";
 import {useTranslation} from "react-i18next";
 import {useEffect, useState} from "react";
-import {fetchActors} from "../../api/actorsApi";
+import {deleteActor, fetchActors} from "../../api/actorsApi";
+import {NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {loggedInUser} from "../../store/slices/userSlice";
+import CustomSnackbar from "../../components/feedback/CustomSnackbar";
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -32,7 +41,6 @@ const useStyles = makeStyles((theme) => ({
     },
     tableRow: {
         textDecoration: 0,
-        cursor: "pointer"
     },
     tableCell: {
         fontSize: theme.spacing(3),
@@ -42,19 +50,61 @@ const useStyles = makeStyles((theme) => ({
     },
     ratingCell: {
         color: theme.palette.secondary.dark
+    },
+    deleteButton: {
+        color: theme.palette.error.main,
+        borderColor: theme.palette.error.main,
+        marginLeft: theme.spacing(2)
+    },
+    buttonColor: {
+        color: theme.palette.secondary.dark,
+        borderColor: theme.palette.secondary.dark,
     }
 }))
 
 const ActorsPage = () => {
     const classes = useStyles()
     const {t} = useTranslation("pages")
+    const currentUser = useSelector(loggedInUser)
 
     const [actors, setActors] = useState([])
+    const [openDeleteActor, setOpenDeleteActor] = useState(false)
+    const [deletableActorId, setDeletableActorId] = useState()
+    const [openDeleteSuccess, setOpenDeleteSuccess] = useState(false)
+    const [openDeleteError, setOpenDeleteError] = useState(false)
 
     useEffect(() => {
         fetchActors()
             .then(({data}) => setActors(data))
     }, [])
+
+    const handleSnackbarClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenDeleteSuccess(false);
+        setOpenDeleteError(false);
+    };
+
+    const initiateActorDelete = (id) => {
+        setDeletableActorId(id);
+        setOpenDeleteActor(true);
+    }
+
+    const postDeleteActor = () => {
+
+        deleteActor(deletableActorId)
+            .then(() => {
+                setOpenDeleteActor(false)
+
+                fetchActors()
+                    .then(({data}) => {
+                        setActors(data)
+                        setOpenDeleteSuccess(true)
+                    })
+            })
+            .catch(() => setOpenDeleteError(true))
+    }
 
     return (
         <>
@@ -66,7 +116,7 @@ const ActorsPage = () => {
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <StyledTableCell colSpan={2} align="left">
+                            <StyledTableCell align="left">
                                 {t('actorSurname')}
                             </StyledTableCell>
 
@@ -77,36 +127,97 @@ const ActorsPage = () => {
                             <StyledTableCell align="right">
                                 {t('actorBirthdate')}
                             </StyledTableCell>
+
+                            {
+                                currentUser?.roles?.includes("ADMIN") &&
+                                <>
+                                    <StyledTableCell align="right">
+                                        {t('actions')}
+                                    </StyledTableCell>
+                                </>
+                            }
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {actors.map((actor) => (
                             <TableRow key={actor.id}
-                                      hover
                                       className={classes.tableRow}>
-                                <TableCell className={classes.imageCell}>
-                                    <CardMedia className={classes.cardMedia} image={actor.photo}
-                                               title={actor.surname}/>
-                                </TableCell>
 
                                 <TableCell align="left" className={classes.tableCell}>
                                     {actor.surname}
                                 </TableCell>
 
-                                <TableCell align="right" className={classes.tableCell}>
+                                <TableCell align="left" className={classes.tableCell}>
                                     {actor.name}
                                 </TableCell>
 
                                 <TableCell align="right" className={`${classes.tableCell} ${classes.ratingCell}`}>
                                     {
-                                        moment(actor.birthDate)?.format("YYYY-mm-DD")
+                                        moment(actor.birthDate)?.format("YYYY-MM-DD")
                                     }
                                 </TableCell>
+
+                                {
+                                    currentUser?.roles?.includes("ADMIN") &&
+                                    <>
+                                        <TableCell align="right"
+                                                   className={`${classes.tableCell} ${classes.ratingCell}`}>
+                                            <Button variant="outlined" className={classes.buttonColor}
+                                                    to={"/actors/edit/" + actor.id}
+                                                    component={NavLink}>
+                                                {t('editActor')}
+                                            </Button>
+                                            <Button variant="outlined" className={classes.deleteButton}
+                                                    onClick={() => initiateActorDelete(actor.id)}>
+                                                {t('removeActor')}
+                                            </Button>
+                                        </TableCell>
+                                    </>
+                                }
+
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            <Dialog
+                open={openDeleteActor}
+                onClose={() => setOpenDeleteActor(false)}
+                aria-labelledby="delete-actor"
+            >
+                <DialogTitle id="delete-actor">
+                    {t('deleteThisActor')}
+                </DialogTitle>
+
+                <DialogActions>
+                    <Button onClick={() => setOpenDeleteActor(false)} color="primary" autoFocus>
+                        {t('cancel')}
+                    </Button>
+                    <Button onClick={() => postDeleteActor()} color="primary">
+                        {t('delete')}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <CustomSnackbar open={openDeleteSuccess}
+                            duration={5000}
+                            handleClose={handleSnackbarClose}
+                            message={t('actorDeleteSuccess')}
+                            elevation={3}
+                            variant="filled"
+                            severity="success"
+            />
+
+            <CustomSnackbar open={openDeleteError}
+                            duration={5000}
+                            handleClose={handleSnackbarClose}
+                            message={t('actorDeleteError')}
+                            elevation={3}
+                            variant="filled"
+                            severity="error"
+            />
+
         </>
     )
 }
